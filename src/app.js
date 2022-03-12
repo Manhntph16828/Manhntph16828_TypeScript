4473ee28e44dc36c57c09cdec9b72c6793891882
Nguyen manh <nguyentienmanhhotboy@gmail.com>
2022-03-12 18:37:23 +0700

    b3 react-typescript

--- src/app.js
+++
@@ -1,5 +0,0 @@
-var b = 30;
-var myName = "nguyen tien manh";
-var myAge = 20;
-var startus = true;
-var myDB = { id: 1, name: "manh" };
---
+++ src/main.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import './index.css'
+// import App from './App'
+const myName : string = "Nguyễn Tiến Mạnh";
+const myStatus : boolean = true;
+const myAge : number = 20;
+const product :{id:number, name: string} = {id:1, name : "Mạnh"}
+type ShowProps = {
+  name : string
+}
+
+ function show (props: ShowProps) : any{
+  console.log(props.name);
+ }
+
+function Show(props : ShowProps) : any {
+  console.log('props',props);
+  console.log(props.name);
+  return null;
+}
+
+ReactDOM.render(<div>
+  <h1>Hello {myName}</h1>
+  <div>{myStatus ? "manh" : "tien"}</div>
+  <div>{myAge}</div>
+  <div>{product.name}</div>
+  <div>{show({name : myName})}</div>
+</div>, document.querySelector('#root'))
